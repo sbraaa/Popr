4c4929197ef6f330dd39df4f97bc0bfe6462a2b1
Domenico Sgarbossa <sgarbossa.domenico@gmail.com>
2021-07-06 11:38:05 +0200

    Add files via upload
    prevent from hiding if beyond right window edge

--- popr/popr.js
+++ popr/popr.js
@@ -1,4 +1,3 @@
-
 /*
 Popr 1.0
 Copyright (c) 2015 Tipue
@@ -7,71 +6,79 @@ http://www.tipue.com/popr
 */
 
 
-(function($) {
+(function ($) {
 
-     $.fn.popr = function(options) {
-     
-          var set = $.extend( {
-               
-               'speed'        : 200,
-               'mode'         : 'bottom'
-          
+     $.fn.popr = function (options) {
+
+          var set = $.extend({
+
+               'speed': 200,
+               'mode': 'bottom'
+
           }, options);
 
-          return this.each(function() {
-          
+          return this.each(function () {
+
                var popr_cont = '.popr_container_' + set.mode;
                var popr_show = true;
 
-               $(this).click(function(event)
-               {
+               $(this).click(function (event) {
                     $('.popr_container_top').remove();
                     $('.popr_container_bottom').remove();
-                    
-                    if (popr_show)
-                    {
+
+                    if (popr_show) {
                          event.stopPropagation();
                          popr_show = false;
-                    }
-                    else
-                    {
+                    } else {
                          popr_show = true;
-                    }                   
-                    
-                    var d_m = set.mode;
-                    if ($(this).attr('data-mode'))
-                    {
-                         d_m = $(this).attr('data-mode')
-                         popr_cont = '.popr_container_' + d_m;   
                     }
-                    
+
+                    var d_m = set.mode;
+                    if ($(this).attr('data-mode')) {
+                         d_m = $(this).attr('data-mode')
+                         popr_cont = '.popr_container_' + d_m;
+                    }
+
                     var out = '<div class="popr_container_' + d_m + '"><div class="popr_point_' + d_m + '"><div class="popr_content">' + $('div[data-box-id="' + $(this).attr('data-id') + '"]').html() + '</div></div></div>';
-                    
+
                     $(this).append(out);
-               
+
                     var w_t = $(popr_cont).outerWidth();
                     var w_e = $(this).width();
                     var m_l = (w_e / 2) - (w_t / 2);
-               
+
                     $(popr_cont).css('margin-left', m_l + 'px');
                     $(this).removeAttr('title alt');
-                    
-                    if (d_m == 'top')
-                    {
+
+                    if (d_m == 'top') {
                          var w_h = $(popr_cont).outerHeight() + 39;
-                         $(popr_cont).css('margin-top', '-' + w_h + 'px');    
+                         $(popr_cont).css('margin-top', '-' + w_h + 'px');
                     }
-                    
-                    $(popr_cont).fadeIn(set.speed);   
+
+                    $(popr_cont).fadeIn(set.speed);
+
+                    setTimeout(() => {
+                         // only if not already shown
+                         if (!popr_show) {
+                              let coords = $(popr_cont)[0].getBoundingClientRect();
+                              let epsilon = 15;
+                              if (window.innerWidth - coords.left - epsilon < w_t) {
+                                   // top right corner (90% circa)     
+                                   $(popr_cont).css('margin-left', '-' + (w_t - (w_e * 2)) + 'px');
+                                   $('.popr_point_'+d_m).addClass('at_corner');
+                              }
+                         }
+                    }, 1);
+
+
                });
-                            
-               $('html').click(function()
-               {
+
+               $('html').click(function () {
                     $('.popr_container_top').remove();
                     $('.popr_container_bottom').remove();
                     popr_show = true;
-               });                           
+               });
           });
      };
-     
+
 })(jQuery);
